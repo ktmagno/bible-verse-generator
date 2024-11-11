8502b205564a6702d738881f5811fbac3cc20c98
ren <160462399+ktmagno@users.noreply.github.com>
2024-11-11 13:38:45 +0800

    Added verse

--- script.js
+++ script.js
@@ -464,7 +464,8 @@ var verse = [
   `He who tills his land will have plenty of bread, but he who follows worthless pursuits will have plenty of poverty\n\n<i>PROVERBS 28:19</i>`,
   `Return to your stronghold, O prisoners of hope; today I declare that I will restore you to double\n\n<i>ZECHARIAH 9:12</i>`,
   `For the needy shall not always be forgotten, and the hope of the poor shall not perish forever\n\n<i>PSALM 9:18</i>`,
-  `For you shall go out in joy, and be led forth in peace; the mountains and the hills before you shall break forth into singing, and all the trees of the field shall clap their hands\n\n<i>ISAIAH 55:12</i>`
+  `For you shall go out in joy, and be led forth in peace; the mountains and the hills before you shall break forth into singing, and all the trees of the field shall clap their hands\n\n<i>ISAIAH 55:12</i>`,
+  `But as for me, I will look to the Lord, I will wait for the God of the salvation; My God will hear me\n\n<i>MICAH 7:7</i>`
 ];
 
 function newVerse() {
